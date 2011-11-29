9d3a0e1b727346502a47e9e07ac33781e9d84f63
alex borsody <asborsody@gmail.com>
2011-11-29 14:48:54 -0500

    replace your own xml feed

---
+++ README.txt
@@ -0,0 +1,36 @@
+jPlayer 2.1.0 Demos
+-------------------
+
+Upload these files to a server online or place on a localhost server,
+otherwise the Flash will encounter security errors if you attempt
+to access the HTML files directly.
+
+
+Directories:
+------------
+
+"js": Contains the jPlayer plugin.
+Also contains the add-ons and CirclePlayer JavaScript.
+If you rename/move this, set the {swfPath:"new/path"} appropriately.
+
+"skin": Contains the Blue Monday and Pink Flag skins
+
+
+Related Links:
+--------------
+
+For Documentation see:
+jPlayer Website: http://www.jplayer.org/
+
+For support requests use:
+jPlayer Google Group: https://groups.google.com/forum/#!forum/jplayer
+
+
+
+PODCAST WIDGET
+--------------
+
+This JPlayer script has been modified to turn it into a podcast widget that lists the latest 3 podcasts
+getxmlforplayer.php
+parses the titles and links to the latest three podcasts and displays them in the player
+Currently we are using Neil Degrasse's Startalk Podcast as a sample.
---
+++ player.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+
+
+<title>Demo : jPlayer as an audio playlist player</title>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+
+
+<link href="http://www.freeenergymedia.com/shared/PLAYER/player/skin/pink.flag/jplayer.pink.flag.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="http://www.freeenergymedia.com/shared/PLAYER/player/js/jquery.jplayer.min.js"></script>
+<script type="text/javascript" src="http://www.freeenergymedia.com/shared/PLAYER/player/js/jplayer.playlist.min.js"></script>
+<script type="text/javascript">
+//<![CDATA[
+$(document).ready(function(){
+
+	new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_1",
+		cssSelectorAncestor: "#jp_container_1"
+	}, <?php include('getxmlforplayer.php') ?>, {
+		swfPath: "js",
+		supplied: "mp3, oga",
+		wmode: "window"
+	});
+});
+//]]>
+</script>
+</head>
+<body>
+<div style="margin:0 auto; padding: 10% 0 0 0; width: 250px;">
+		<div id="jquery_jplayer_1" class="jp-jplayer"></div>
+
+		<div id="jp_container_1" class="jp-audio">
+			<div class="jp-type-playlist">
+				<div class="jp-gui jp-interface">
+					<ul class="jp-controls">
+						<li><a href="javascript:;" class="jp-previous" tabindex="1">previous</a></li>
+						<li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
+						<li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
+						<li><a href="javascript:;" class="jp-next" tabindex="1">next</a></li>
+						<li><a href="javascript:;" class="jp-stop" tabindex="1">stop</a></li>
+						<li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
+						<li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
+						<li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
+					</ul>
+					<div class="jp-progress">
+						<div class="jp-seek-bar">
+							<div class="jp-play-bar"></div>
+
+						</div>
+					</div>
+					<div class="jp-volume-bar">
+						<div class="jp-volume-bar-value"></div>
+					</div>
+					<div class="jp-current-time"></div>
+					<div class="jp-duration"></div>
+					<ul class="jp-toggles">
+						<li><a href="javascript:;" class="jp-shuffle" tabindex="1" title="shuffle">shuffle</a></li>
+						<li><a href="javascript:;" class="jp-shuffle-off" tabindex="1" title="shuffle off">shuffle off</a></li>
+						<li><a href="javascript:;" class="jp-repeat" tabindex="1" title="repeat">repeat</a></li>
+						<li><a href="javascript:;" class="jp-repeat-off" tabindex="1" title="repeat off">repeat off</a></li>
+					</ul>
+				</div>
+				<div class="jp-playlist">
+					<ul>
+						<li></li>
+					</ul>
+				</div>		
+				<div class="jp-no-solution">
+					<span>Update Required</span>
+					To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+				</div>
+			</div>
+		</div>
+</body>
+
+</html>
